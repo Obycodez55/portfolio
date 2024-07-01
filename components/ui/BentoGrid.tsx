1030d0f08925e998e83365d45509539df61b0125
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2024-07-01 08:16:37 +0000

    Completed Grid designs

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,6 +1,13 @@
+"use client"
+
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./GradientBg";
 import { GridGlobe } from "./GridGlobe";
+import { useCallback, useState } from "react";
+import animationData from "../../data/confetti.json";
+import Lottie from "react-lottie";
+import MagicButton from "./MagicButton";
+import { IoCopyOutline } from "react-icons/io5";
 
 export const BentoGrid = ({
   className,
@@ -43,6 +50,24 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy= useCallback(()=>{
+    navigator.clipboard.writeText("obikoyaadebayo55@gmail.com");
+    setCopied(true);
+  }, [])
+
+  const leftList = ["React.js", "Next.js", "Typescript"];
+  const rightList = ["Vue.js", "MongoDb", "NodeJS"];
+  const lottieOptions = {
+    loop: copied,
+    autoplay: copied,
+    animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  }
+
   return (
     <div
       className={cn(
@@ -92,30 +117,32 @@ export const BentoGridItem = ({
           <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base z-10">
             {description}
           </div>
-          <div className="font-sans font-bold text-lg lg:text3x max-w-96 z-10">
+          <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
             {title}
           </div>
           {id === 2 && <GridGlobe />}
 
           {id === 3 && (
             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-              <div className="flex flex-col gap-3 lg:gap-8">
-                {["React.js", "Next.js", "Typescript"].map((item) => (
+              <div className="flex flex-col gap-3">
+                {leftList.map((item) => (
                   <span
                     key={item}
-                    className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                    className="py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
                   >
                     {item}
                   </span>
                 ))}
                 <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
               </div>
-              <div className="flex flex-col gap-3 lg:gap-8">
+              <div className="flex flex-col gap-3">
                 <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
-                {["Vue.js", "MongoDb", "NodeJS"].map((item) => (
+                {rightList.map((item) => (
                   <span
                     key={item}
-                    className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                    className="py-2 px-3 text-xs lg:text-base opacity-50 
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
                   >
                     {item}
                   </span>
@@ -123,6 +150,23 @@ export const BentoGridItem = ({
               </div>
             </div>
           )}
+      {id === 6 && (
+        <div className="mt-5 relative">
+          <div className={`absolute -bottom-5 right-0`}>
+            <Lottie options={lottieOptions} />
+          </div>
+
+            <MagicButton 
+              title={copied ? "Email copied" : "Copy My email"}
+              icon={<IoCopyOutline />}
+              position="left"
+              otherClasses="!bg-[#161A31]"
+              handleClick={handleCopy}
+            />
+
+        </div>
+      )}
+
         </div>
       </div>
     </div>
