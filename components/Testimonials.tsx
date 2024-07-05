a25577080da7ebd06397671e76852218a1eaad8f
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2024-07-05 17:53:10 +0000

    feat: Add company logos to Testimonials component

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -1,22 +1,38 @@
 import React from "react";
 import { InfiniteMovingCards } from "./ui/InfiniteMovingCards";
-import { testimonials } from "@/data";
+import { companies, testimonials } from "@/data";
 
 type Props = {};
 
 const Testimonials = (props: Props) => {
   return (
-    <div className="py-20" id="projects">
+    <div className="py-20" id="testimonials">
       <h1 className="heading">
-       Kind Words From{" "}
-        <span className="text-purple"> Satisfied Clients</span>
+        Kind Words From <span className="text-purple"> Satisfied Clients</span>
       </h1>
       <div className="flex flex-col items-center max-lg:mt-10">
         <InfiniteMovingCards
-            items={testimonials}
-            direction="right"
-            speed="slow"
-            />
+          items={testimonials}
+          direction="right"
+          speed="slow"
+        />
+
+        <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
+          {companies.map(({id, img, name, nameImg}, idx) => (
+            <div key={id} className="flex md:max-w-60 max-w-32 gap-2">
+              <img 
+              src={img} 
+              alt={name} 
+              className="md:w-10 w-5"
+              />
+              <img 
+              src={nameImg} 
+              alt={name} 
+              className="md:w-24 w-20"
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
