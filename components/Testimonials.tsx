c66ce901cdf8a4e39eb5347f345ff548c4f5b019
Obikoya Adebayo <obikoyaadebayo55@gmail.com>
2024-07-05 17:42:27 +0100

    feat: Add Testimonials component to home page

---
+++ components/Testimonials.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { InfiniteMovingCards } from "./ui/InfiniteMovingCards";
+import { testimonials } from "@/data";
+
+type Props = {};
+
+const Testimonials = (props: Props) => {
+  return (
+    <div className="py-20" id="projects">
+      <h1 className="heading">
+       Kind Words From{" "}
+        <span className="text-purple"> Satisfied Clients</span>
+      </h1>
+      <div className="flex flex-col items-center">
+        <div className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center relative overflow-hidden">
+        <InfiniteMovingCards
+            items={testimonials}
+            direction="right"
+            speed="slow"
+            />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
